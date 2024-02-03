fd4bb950d9e285586e3a39b702f9f916c3b209a0
silizard <wkbkmj1@naver.com>
2024-02-03 20:34:58 +0900

    Create Register System

--- DB_handle.py
+++ DB_handle.py
@@ -5,5 +5,25 @@ class DBModule:
     def __init__(self):
         with open("./auth/auth.json") as f:
             config = json.load(f)
-         
-        self.firebase = pyrebase.initialize_app(config)
+        firebase = pyrebase.initialize_app(config)
+        self.db = firebase.database()
+    
+    def register_verification(self, id_):
+        users = self.db.child("users").get().val()
+        for user_id in users.keys():
+            if id_ == user_id:
+                return False
+        return True
+
+
+    def register(self, id_, password, name, email):
+        user_data = { 
+            "user_password": password,
+            "user_name": name,
+            "user_email": email
+        }
+        if self.register_verification(id_):
+            self.db.child("users").child(id_).set(user_data)
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
+from DB_handle import DBModule
 
 app = Flask("BulletinBoard")
+app.secret_key = "Bulletin_Board_Skey"
+DB = DBModule()
 
 @app.route("/")
 def home():
@@ -17,5 +20,17 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/register_action", methods=["get"])
+def register_action():
+    user_id = request.args.get("id")
+    user_passward = request.args.get("password")
+    user_name = request.args.get("name")
+    user_email = request.args.get("email")
+    if DB.register(user_id, user_passward, user_name, user_email):
+        return redirect(url_for("home"))
+    else:
+        flash("이미 존재하는 아이디 입니다.")
+        return redirect(url_for("register"))
 
 app.run("127.0.0.1", debug=True)
